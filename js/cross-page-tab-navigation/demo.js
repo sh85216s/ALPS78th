11f5c3888ad6a3eaef936bac06522c69f7b1ab20
shiarng <88964278+sh85216s@users.noreply.github.com>
2023-01-24 21:34:15 +0800

    test
    test

---
+++ js/cross-page-tab-navigation/demo.js
@@ -0,0 +1,19 @@
+/* defaults: 
+   tabs: [
+     { id: 'demoTab1', label: 'Demo Tab 1', url: '#demoTab1'},
+     { id: 'demoTab2', label: 'Demo Tab 2', url: '#demoTab2'}
+   ]
+*/
+let tabsConfig = {
+    tabs: [
+        {
+            id: 'demoTab1',
+            label: 'Demo Tab 1',
+            url: 'index.html',
+            tooltip: 'Demo 1',
+            tabClass: 'customTabClass',
+            spacerClass: 'customSpacerClass',
+        },
+        // ...
+    ]
+};
